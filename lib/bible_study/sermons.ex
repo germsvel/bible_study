615f1e2ffd5cb92f34ab0182ad939a84dc2a6717
German Velasco <germsvel@gmail.com>
2016-03-08 09:03:33 -0500

    Add first implementation of BibleStudy.Sermons.TheGospelCoalition module. Need to improve the scripture reference parsing

--- lib/bible_study/sermons.ex
+++ lib/bible_study/sermons.ex
@@ -1,9 +1,10 @@
 defmodule BibleStudy.Sermons do
   alias BibleStudy.Sermons.DesiringGod
   alias BibleStudy.Sermons.UniversityReformedChurch, as: URC
+  alias BibleStudy.Sermons.TheGospelCoalition, as: TGC
 
   def find(passage) do
-    [DesiringGod, URC]
+    [DesiringGod, URC, TGC]
     |> Enum.map(&Task.async(&1, :find, [passage]))
     |> Enum.map(&Task.await(&1))
     |> List.flatten
---
+++ lib/bible_study/sermons/the_gospel_coalition.ex
@@ -0,0 +1,87 @@
+defmodule BibleStudy.Sermons.TheGospelCoalition do
+  alias BibleStudy.StudyResource, as: Resource
+
+  @base_url "http://resources.thegospelcoalition.org"
+
+  def find(passage) do
+    generate_url(passage)
+    |> request_page()
+    |> process_response()
+    |> filter_resources_by_passage(passage)
+    |> IO.inspect
+  end
+
+  defp generate_url(passage) do
+    book = String.capitalize(passage.book)
+    chapter = book <> "+" <> passage.chapter
+    "#{@base_url}/library?f[book][]=#{book}&f[chapter][]=#{chapter}&f[language][]=English&f[resource_category][]=Sermons"
+  end
+
+  defp request_page(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+          IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+          IO.inspect reason
+      _ ->
+        IO.puts "Could not find #{url}"
+    end
+  end
+
+  defp process_response(nil), do: ""
+  defp process_response(body) do
+    Floki.find(body, ".blacklight_list-item")
+    |> Enum.map(&create_resource/1)
+  end
+
+  defp create_resource(html_tree) do
+    %Resource{type: :sermon}
+    |> add_title(html_tree)
+    |> add_url(html_tree)
+    |> add_scripture_ref(html_tree)
+    |> add_date(html_tree)
+    |> add_author(html_tree)
+  end
+
+  defp add_url(resource, html_tree) do
+    [relative_url] = Floki.find(html_tree, "a")
+                  |> Floki.attribute("href")
+                  |> Enum.take(1)
+
+    url = @base_url <> relative_url
+    %{resource | url: url}
+  end
+  defp add_title(resource, html_tree) do
+    %{resource | title: search_tree(html_tree, "h3") }
+  end
+  defp add_scripture_ref(resource, html_tree) do
+    [_, ref] = html_tree |> get_date_and_scripture()
+    [ref] = ref |> String.split(";") |> Enum.filter(fn (x) -> String.contains?(x, "Romans") end) |> Enum.take(1)
+    ref = String.strip(ref)
+    %{resource | scripture_reference: ref}
+  end
+  defp add_date(resource, html_tree) do
+    [date, _] = html_tree |> get_date_and_scripture()
+    %{resource | date: date}
+  end
+  defp add_author(resource, html_tree) do
+    %{resource | author: search_tree(html_tree, "h4")}
+  end
+
+  defp get_date_and_scripture(html_tree) do
+    search_tree(html_tree, ".scripture_ref") |> String.split("|") |> Enum.map(&String.strip/1)
+  end
+
+  defp search_tree(html_tree, css_selector) do
+    Floki.find(html_tree, css_selector) |> Floki.text |> String.strip
+  end
+
+  defp filter_resources_by_passage(sermons, passage) do
+    Enum.filter(sermons, fn(sermon) ->
+      Resource.relevant_for_passage?(sermon, passage)
+    end)
+  end
+
+end
