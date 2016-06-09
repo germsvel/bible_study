146694a3583f7a9a5e0756156700e38bc2515e89
German Velasco <germsvel@gmail.com>
2016-06-09 07:41:00 -0400

    Update sermons mock to comply with changes in sermon interface

--- lib/bible_study/sermons/mock.ex
+++ lib/bible_study/sermons/mock.ex
@@ -1,7 +1,12 @@
 defmodule BibleStudy.Sermons.Mock do
   alias BibleStudy.StudyResource, as: Resource
 
-  def find(_passage) do
-    %Resource{type: :sermon, author: "John Piper"}
+  def start_link(passage, sermon_ref, owner) do
+    Task.start_link(__MODULE__, :find, [passage, sermon_ref, owner])
+  end
+
+  def find(_passage, sermon_ref, owner) do
+    results = [%Resource{type: :sermon, author: "John Piper"}]
+    send(owner, {:results, sermon_ref, results})
   end
 end
